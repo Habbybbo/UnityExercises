1fc66f42c9abf6687fc122b414f3aa630a19f059
Yuliyan Andreev <habybbo@gmail.com>
2022-09-20 21:44:53 +0300

    FlyBy v1.0
    Flying through obstacles.

---
+++ FlyBy/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,57 @@
+
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] float timeToDelay = 2f;
+
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+            Debug.Log("You'v bumped into friendly");
+            break;
+            case "Finish":
+            StartSuccessSequence(timeToDelay);
+            break;
+            default:
+            StartCrashSequence(timeToDelay);
+            break;
+        }
+    }
+
+    private void StartSuccessSequence(float delayTime)
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextScene", delayTime);
+    }
+
+    void StartCrashSequence(float delayTime)
+    {
+        GetComponent<Movement>().enabled = false;
+
+        Invoke("ReloadScene", delayTime);
+    }
+
+    void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
---
+++ FlyBy/Assets/Scripts/Movement.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    AudioSource audioSource;
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationThrust = 100f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+
+    void ProcessThrust()
+    {
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            AddRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            AddRotation(-rotationThrust);
+        }
+    }
+
+    private void AddRotation(float rotationPerFrame)
+    {
+        rb.freezeRotation = true; //freeze the manual rotation
+        transform.Rotate(Vector3.forward * rotationPerFrame * Time.deltaTime);
+        rb.freezeRotation = false; // unfreeze the manual rotation
+    }
+}
