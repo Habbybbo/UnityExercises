289535d6bc0edee921c713764ff766b2622e018a
Yuliyan Andreev <habybbo@gmail.com>
2022-10-30 19:20:16 +0200

    ShootThemAll!!! 2.0
    Adding scoreboard, enemy vfx, player vfx

--- ShootThemAll!/Assets/Scripts/Enemy.cs
+++ ShootThemAll!/Assets/Scripts/Enemy.cs
@@ -4,8 +4,56 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deatVFX;
+    [SerializeField] int scorePerHit;
+    [SerializeField] int hitPoints;
+    [SerializeField] GameObject hitVFX;
+
+    ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        AddRigidbody();
+    }
+
+    private void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
     void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+        
+    }
+
+    private void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deatVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(this.gameObject);
     }
-}
+
+    private void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
+
+        hitPoints--;
+
+        if (hitPoints < 1)
+        {
+            scoreBoard.IncreaseScore(scorePerHit);
+        }
+        
+    }
+} 
